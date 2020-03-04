293bc81a551c48623427891ae2189272126cc854
Chuck <bnsf5622@gmail.com>
2020-03-04 18:58:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace using_visual_studio_debugging_tools
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Forgot a semi colon!");
+            {
+            }
+                int a = 5;
+            { 
+                int b = 10;
+            }
+            return;
+            {
+                int sum = a + b;
+            }
+
+            public static void main(string[] arg)
+
+                int[] number = new int[] { 3, 1, 4 };
+
+            for (int i = 0; i <= numbers.Length; i++) 
+            {
+                Console.WriteLine($"The number at index {i} is {numbers[i]}.");
+
+            }
+        }
+    }
+}
